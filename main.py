1b219f069204104a9f96761c771281dae3d7efa1
manthanparekh <manthan.parekh@outlook.com>
2022-11-23 13:51:43 -0500

    finish welcome page

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-# TODO-1: Show inventory and prices
-# TODO-2: repeat  asking user for inputs  until Done
-# TODO-3: end task when user inputs 'done
-# TODO-4: print total items and cost when done
-# TODO-5: use dictionary, functions, loops and conditionals
+# TODO-1: Show inventory and prices  ------------------------- DONE
+# TODO-2: repeat  asking user for inputs  until Done ---------
+# TODO-3: end task when user inputs 'done --------------------
+# TODO-4: print total items and cost when done ---------------
+# TODO-5: use dictionary, functions, loops and conditionals --
 
-print("hello")
+# printing out welcome page with information
+print("-----Welcome to the Ash's Grocery store!-----")
+print("Please choose from the selected items below: ")
+
+items = {
+    'Bread' : 3.5,
+    'Milk' : 2,
+    'Cake' : 7,
+    'Corona-Extra' : 10,
+    'Coffee' : 15,
+    'linux' : 0
+}
+print(items)
+
+
