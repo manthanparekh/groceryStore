c08d70441ddcc0a5df3d242dc510ed006d42d024
Manthan Parekh <manthan.parekh@outlook.com>
2022-11-23 12:51:27 -0500

    added todos

---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO-1: Show inventory and prices
+# TODO-2: repeat  asking user for inputs  until Done
+# TODO-3: end task when user inputs 'done
+# TODO-4: print total items and cost when done
+# TODO-5: use dictionary, functions, loops and conditionals
+
+print("hello")
